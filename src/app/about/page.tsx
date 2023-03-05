5c60a3ca0bad8bb4db13ad73f9eeb71ec7e8b856
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-03-05 03:54:48 +0100

    text animation added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,18 +10,18 @@ const page = () => {
           building things that live on the internet. <br />
           My experience as a programmer allows me to come up with{" "}
           <Link href="/recentwork">
-            <span className="underline text-[#d6d6d6]">
+            <span className="underline text-[#d6d6d6] hover:text-gray-100">
               intelligent solutions
             </span>
           </Link>{" "}
           to technical challenges, while at the same time designing sleek and
-          visually appealing websites. Aside from having extensive knowledge of
+          visually appealing websites. Aside from having <span className="hover:text-gray-100">extensive knowledge</span> of
           recognized technical standards, I am conversant with modern building
           practices.
           <br />
           <br /> I have extensive knowledge of CMS such as Shopify, WordPress,
-          Wix, and Webflow and modern web technologies including: Typescript,
-          React Js, Next Js, Javascript (ES6+) Sanity and Tailwind CSS.
+          Wix, and Webflow and modern web technologies including: <span className="hover:text-gray-100">Typescript</span>,
+          <span className="hover:text-gray-100">React Js</span>, <span className="hover:text-gray-100">Next Js</span>, <span className="hover:text-gray-100">Javascript (ES6+)</span> <span className="hover:text-gray-100">Sanity</span> and <span className="hover:text-gray-100">Tailwind CSS</span>.
         </div>
         <div className="max-w-md pt-32 mx-auto">
           <Image
