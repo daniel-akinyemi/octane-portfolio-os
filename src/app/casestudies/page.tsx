145433b2369571b4348d485e52cfd0e621a0fa9c
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-03-07 13:41:56 +0100

    background image props added in case studies page

--- src/app/casestudies/page.tsx
+++ src/app/casestudies/page.tsx
@@ -1,15 +1,17 @@
 import ProjectCard from "../../../components/ProjectCard"
+import octaneHero from '../../../public/octanehero.png'
+
 
 const page = () => {
   return (
     <div className="scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-slate-700 mx-auto">
       <div className="max-w-5xl mx-auto py-20 grid sm:grid-cols-2 grid-cols-1 lg:grid-cols-3">
-            <ProjectCard projectName="Octane Portfolio OS" projectSummary="Modern developer portfolio website" techCard1="React" techCard2="Tailwind CSS" techCard3="Javascript"/>
-            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS"/>
-            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel"/>
-            <ProjectCard projectName="Octane Portfolio OS" projectSummary="Modern developer portfolio website" techCard1="React" techCard2="Tailwind CSS" techCard3="Javascript"/>
-            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS"/>
-            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel"/>
+            <ProjectCard projectName="Octane Portfolio OS" projectSummary="Modern developer portfolio website" techCard1="React" techCard2="Tailwind CSS" techCard3="Javascript" projectLink="#"/>
+            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS" projectLink="https://souqify.com" backgroundImage={`${octaneHero.src}`}/>
+            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel" projectLink="#"/>
+            <ProjectCard projectName="Octane Portfolio OS" projectSummary="Modern developer portfolio website" techCard1="React" techCard2="Tailwind CSS" techCard3="Javascript" projectLink="#"/>
+            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS" projectLink="#"/>
+            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel" projectLink="#"/>
         
         </div>
     </div>
