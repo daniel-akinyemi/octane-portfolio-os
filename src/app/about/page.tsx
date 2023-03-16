4f4be63b49511e58fca3afb153c8368bdbbeaff9
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-03-16 23:24:17 +0100

    portfolio projects page updated, focus styling updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ const page = () => {
       className="max-w-md text-[#d2d2d2] pt-32"
       >
         <div className="px-6 sm:px-0">
-          <span className="font-bold text-gray-100">Hello!</span> My name is Daniel and I love
+          <span className="font-regular text-gray-100">Hi!</span> My name is Daniel and I love
           building things that live on the internet. <br />
           My experience as a programmer allows me to come up with{" "}
           <Link href="/casestudies">
--- src/app/casestudies/page.tsx
+++ src/app/casestudies/page.tsx
@@ -1,5 +1,7 @@
 import ProjectCard from "../../../components/ProjectCard"
 import octaneHero from '../../../public/octanehero.png'
+import chatgptclonehomepage from '../../../public/chatgptclonehomepage.png'
+import souqifyhomepage from '../../../public/souqify-homepage.png'
 
 
 const page = () => {
@@ -7,11 +9,11 @@ const page = () => {
     <div className="scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-slate-700 mx-auto">
       <div className="max-w-5xl mx-auto py-20 grid sm:grid-cols-2 grid-cols-1 lg:grid-cols-3">
             <ProjectCard projectName="Octane Portfolio OS" projectSummary="Modern developer portfolio website" techCard1="React" techCard2="Tailwind CSS" techCard3="Javascript" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
-            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS" projectLink="https://souqify.com" backgroundImage={`${octaneHero.src}`}/>
-            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
-            <ProjectCard projectName="Octane Portfolio OS" projectSummary="Modern developer portfolio website" techCard1="React" techCard2="Tailwind CSS" techCard3="Javascript" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
-            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
-            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
+            <ProjectCard projectName="Souqify" projectSummary="Your plug for house finishing products" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS" projectLink="https://souqify.com" backgroundImage={`${souqifyhomepage.src}`}/>
+            <ProjectCard projectName="Chat GPT Clone 1.1" projectSummary="I created a clone of the popular AI chatbot" techCard1="NextJS" techCard2="Tailwind CSS" techCard3="Vercel" projectLink="https://chat-gpt-clone-1-1-git-main-daniel-akinyemi.vercel.app/" backgroundImage={`${chatgptclonehomepage.src}`}/>
+            <ProjectCard projectName="De-Fi Trading WebApp" projectSummary="Landing Page for a De-Fi trading firm" techCard1="React" techCard2="Tailwind CSS" techCard3="Vercel" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
+            <ProjectCard projectName="Google Clone 2.0" projectSummary="modern clone of Google search with React Js" techCard1="Next.JS" techCard2="Tailwind" techCard3="Vercel" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
+            <ProjectCard projectName="Prisma Shopify Theme" projectSummary="I created a clone of the popular AI chatbot" techCard1="Liquid" techCard2="Javascript" techCard3="CSS/SCSS" projectLink="https://danielakinyemi.vercel/app/casestudies" backgroundImage={`${octaneHero.src}`}/>
         
         </div>
     </div>
