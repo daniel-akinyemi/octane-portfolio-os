53143f9e743601855a55748671a4b2ad4a681cff
daniel-akinyemi <danielakinyemi1999@gmail.com>
2024-02-07 16:00:14 +0100

    add: mobile logo, mobile nav updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,22 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Gitlogo from "../public/githublogo.svg";
 import LinkedinLogo from "../public/linkedinlogo.svg";
-import { ArrowDownOnSquareStackIcon, XMarkIcon, Bars2Icon } from "@heroicons/react/24/outline";
+import image from "../public/image.png";
+import {
+  ArrowDownOnSquareStackIcon,
+  XMarkIcon,
+  Bars2Icon,
+} from "@heroicons/react/24/outline";
 import { useState } from "react";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-  const [nav,setNav] = useState(false)
-  
-  const handleNav =()=>{
-    setNav(!nav)
-  }
-
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
     <div className="relative">
@@ -58,40 +61,44 @@ const Navbar = () => {
         </div>
       </div>
 
+      <div className="mobile-nav flex justify-between max-w-[90%] mx-auto md:hidden">
+      <Image src={image.src} width={100} height={100} alt="logo" />
         {/* Mobile Menu Button */}
-      <div className="block md:hidden my-8 px-4 cursor-crosshair z-10 top-0"
-      onClick={handleNav}
-      >
-      { nav ? <XMarkIcon className="text-white h-5 w-5"/> : <Bars2Icon className="text-white h-5 w-5"/>}
+        <div
+          className="block  my-8 px-4 cursor-crosshair z-10 top-0"
+          onClick={handleNav}
+        >
+          {nav ? (
+            <XMarkIcon className="text-white h-5 w-5" />
+          ) : (
+            <Bars2Icon className="text-white h-5 w-5" />
+          )}
+        </div>
+        
       </div>
 
-        {/* Mobile Menu */}
-      <div className={nav ? 'absolute mobilemenu bg-black h-[100vh]' : ' absolute left-[-100%] ease-out duration-200'}>
-        <Link href="/" className="hovercode"
-        onClick={handleNav}
-        >
+      {/* Mobile Menu */}
+      <div
+        className={
+          nav
+            ? "absolute mobilemenu bg-black h-[100vh]"
+            : " absolute left-[-100%] ease-out duration-200"
+        }
+      >
+        <Link href="/" className="hovercode" onClick={handleNav}>
           Home
-          
         </Link>
-        <Link href="/about" className={`hovercode`}
-        onClick={handleNav}
-        >
+        <Link href="/about" className={`hovercode`} onClick={handleNav}>
           About
         </Link>
-        <Link href="/casestudies" className="hovercode"
-        onClick={handleNav}
-        >
+        <Link href="/casestudies" className="hovercode" onClick={handleNav}>
           Case Studies
         </Link>
 
-        <Link href="/blog" className="hovercode"
-        onClick={handleNav}
-        >
+        <Link href="/blog" className="hovercode" onClick={handleNav}>
           Blog
         </Link>
-        <Link href="/getintouch" className="hovercode"
-        onClick={handleNav}
-        >
+        <Link href="/getintouch" className="hovercode" onClick={handleNav}>
           Get In Touch
         </Link>
         <div className="flex">
